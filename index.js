557c813fcd0e3dfdcf32e76d24a899d628e857d1
shinhyo26okazaki <shibo6937@gmail.com>
2022-11-30 13:37:37 -0400

    Add blue style to nav list

--- index.js
+++ index.js
@@ -91,16 +91,25 @@ function showList() {
   sectionList.classList.remove('hide');
   sectionAdd.classList.add('hide');
   sectionContact.classList.add('hide');
+  navList.classList.add('blue');
+  navAdd.classList.remove('blue');
+  navContact.classList.remove('blue');
 }
 function showAdd() {
   sectionList.classList.add('hide');
   sectionAdd.classList.remove('hide');
   sectionContact.classList.add('hide');
+  navList.classList.remove('blue');
+  navAdd.classList.add('blue');
+  navContact.classList.remove('blue');
 }
 function showContact() {
   sectionList.classList.add('hide');
   sectionAdd.classList.add('hide');
   sectionContact.classList.remove('hide');
+  navList.classList.remove('blue');
+  navAdd.classList.remove('blue');
+  navContact.classList.add('blue');
 }
 
 navList.onclick = showList;
